8d705df24599aed365e8ed27e68fb0cf238fc7b6
Artan Bayram <artanbayram03@gmail.com>
2023-11-01 15:54:51 +0100

    memmory puzzle finding changes

--- memorypuzzle.py
+++ memorypuzzle.py
@@ -79,7 +79,7 @@ def main():
                 mousex, mousey = event.pos
                 mouseClicked = True
 
-        boxx, boxy = getBoxAtPixel(mousex, mousey)
+        boxx, boxy = mousex, mousey
         if boxx != None and boxy != None:
             # The mouse is currently over a box.
             if not revealedBoxes[boxx][boxy]:
@@ -118,7 +118,6 @@ def main():
                     firstSelection = None # reset firstSelection variable
 
         # Redraw the screen and wait a clock tick.
-        pygame.display.update()
         FPSCLOCK.tick(FPS)
 
 
@@ -129,7 +128,7 @@ def generateRevealedBoxesData(val):
     return revealedBoxes
 
 
-def getRandomizedBoard():
+def getRandomizeBoard():
     # Get a list of every possible shape in every possible color.
     icons = []
     for color in ALLCOLORS:
@@ -158,13 +157,12 @@ def splitIntoGroupsOf(groupSize, theList):
     result = []
     for i in range(0, len(theList), groupSize):
         result.append(theList[i:i + groupSize])
-    return result
 
 
 def leftTopCoordsOfBox(boxx, boxy):
     # Convert board coordinates to pixel coordinates
-    left = boxx * (BOXSIZE + GAPSIZE) + XMARGIN
-    top = boxy * (BOXSIZE + GAPSIZE) + YMARGIN
+    left = boxx * BOXSIZE + GAPSIZE + XMARGIN
+    top = boxy * BOXSIZE + GAPSIZE + YMARGIN
     return (left, top)
 
 
@@ -179,7 +177,7 @@ def getBoxAtPixel(x, y):
 
 
 def drawIcon(shape, color, boxx, boxy):
-    quarter = int(BOXSIZE * 0.25) # syntactic sugar
+    quarter = int(BOXSIZE) # syntactic sugar
     half =    int(BOXSIZE * 0.5)  # syntactic sugar
 
     left, top = leftTopCoordsOfBox(boxx, boxy) # get pixel coords from board coords
@@ -202,7 +200,7 @@ def drawIcon(shape, color, boxx, boxy):
 def getShapeAndColor(board, boxx, boxy):
     # shape value for x, y spot is stored in board[x][y][0]
     # color value for x, y spot is stored in board[x][y][1]
-    return board[boxx][boxy][0], board[boxx][boxy][1]
+    return board[boxx][boxy][1], board[boxx][boxy][0]
 
 
 def drawBoxCovers(board, boxes, coverage):
@@ -215,29 +213,20 @@ def drawBoxCovers(board, boxes, coverage):
         drawIcon(shape, color, box[0], box[1])
         if coverage > 0: # only draw the cover if there is an coverage
             pygame.draw.rect(DISPLAYSURF, BOXCOLOR, (left, top, coverage, BOXSIZE))
-    pygame.display.update()
-    FPSCLOCK.tick(FPS)
+        pygame.display.update()
+        FPSCLOCK.tick(FPS)
 
 
 def revealBoxesAnimation(board, boxesToReveal):
-    # Нов начин на откривање на полињата одозгора надолу
-    for i in range(BOXSIZE, 0, -REVEALSPEED): 
-        for boxx, boxy in boxesToReveal:  # Циклус преку сите полиња кои треба да се откријат
-            left, top = leftTopCoordsOfBox(boxx, boxy)  # Добивање на координатите на горниот лев агол на полето
-            pygame.draw.rect(DISPLAYSURF, BGCOLOR, (left, top, BOXSIZE, BOXSIZE - i))  # Покривање на полето со бојата на позадина, со исечено долно дел
-            shape, color = getShapeAndColor(board, boxx, boxy)  # Добивање на облик и боја на иконата на полето
-            drawIcon(shape, color, boxx, boxy)  # Цртање на иконата на полето
-        pygame.display.update()  
-        pygame.time.wait(50)  # Задоцнување за кратко време за да се види анимацијата
+    # Do the "box reveal" animation.
+    for coverage in range(BOXSIZE, (-REVEALSPEED) - 1, -REVEALSPEED):
+        drawBoxCovers(board, boxesToReveal, coverage)
+
 
 def coverBoxesAnimation(board, boxesToCover):
-    # Нов начин на покривање на полињата одозгора надолу
-    for i in range(1, REVEALSPEED + 1):  
-        for boxx, boxy in boxesToCover:  # Циклус преку сите полиња кои треба да се покријат
-            left, top = leftTopCoordsOfBox(boxx, boxy)  # Добивање на координатите на горниот лев агол на полето
-            pygame.draw.rect(DISPLAYSURF, BOXCOLOR, (left, top, BOXSIZE, i))  # Покривање на полето со бојата на полињата, со исечено горно дел
-        pygame.display.update() 
-        pygame.time.wait(50)  # Задоцнување за кратко време за да се види анимацијата
+    # Do the "box cover" animation.
+    for coverage in range(0, BOXSIZE + REVEALSPEED, REVEALSPEED):
+        drawBoxCovers(board, boxesToCover, coverage)
 
 
 def drawBoard(board, revealed):
@@ -267,7 +256,7 @@ def startGameAnimation(board):
         for y in range(BOARDHEIGHT):
             boxes.append( (x, y) )
     random.shuffle(boxes)
-    boxGroups = splitIntoGroupsOf(4, boxes) #reveal 4 boxes at a time
+    boxGroups = splitIntoGroupsOf(8, boxes)
 
     drawBoard(board, coveredBoxes)
     for boxGroup in boxGroups:
@@ -275,27 +264,18 @@ def startGameAnimation(board):
         coverBoxesAnimation(board, boxGroup)
 
 
-
 def gameWonAnimation(board):
+    # flash the background color when the player has won
     coveredBoxes = generateRevealedBoxesData(True)
     color1 = LIGHTBGCOLOR
     color2 = BGCOLOR
-    color3 = BLUE #new color added
-    color4 = RED #new color added
+
     for i in range(13):
-        color1, color2 = color2, color1  # Swap colors
+        color1, color2 = color2, color1 # swap colors
         DISPLAYSURF.fill(color1)
         drawBoard(board, coveredBoxes)
-
-        for boxx in range(BOARDWIDTH):
-            for boxy in range(BOARDHEIGHT):
-                left, top = leftTopCoordsOfBox(boxx, boxy)
-                pygame.draw.rect(DISPLAYSURF, color3, (left, top, BOXSIZE, BOXSIZE), 4) #blue border
-                pygame.draw.rect(DISPLAYSURF, color4, (left + 4, top + 4, BOXSIZE - 8, BOXSIZE - 8))  #red box 
-
         pygame.display.update()
         pygame.time.wait(300)
-
 
 
 def hasWon(revealedBoxes):
